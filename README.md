4e2958332f9df80b8956e082fa63a6f158d2be06
Sai MarapaReddy <sai.marapareddy@gmail.com>
2017-09-28 15:38:38 -0400

    Text similarity code includes UI & README file
    An overview of implmentation:
    Sentences are fragmented in to words.
    using Google News word2vec, vectors are calculated.
    Average of vectors are taken for each sentence.
    Using cosine similarity, similar sentences are found.
    
    UI: Html pages, using flask. It generates a simple
    web application. It helps in viewing the similar sentences easily.
    
    Enhancements: During data collection process some sentences may not
    contain any vectors from 'Google vector model'. They are randomnly
    placed in vector space. The count of sentences that don't have any
    vectors are few.
    
    Author:  Sai MarapaReddy <sai.marapareddy@gmail.com>
    Committer: Sai MarapaReddy <sai.marapareddy@gmail.com>

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # TextSimilarity
+helper_functions_v1:
+
+Includes model, helper functions.
+Inputs: a pkl file with skill_name
+No ouput expected.
+
+sentence_similarity.py:
+
+Includes python functions that calls helper functions and returns output.
+Inputs: Input sentence (for which you need to find similar sentences)
+Output: Five most similar sentences where each sentence has agent id/ company id / cosine similarity measure
---
+++ sentence_similarity.py
@@ -0,0 +1,37 @@
+from flask import Flask, render_template, flash, request
+from wtforms import Form, TextField, TextAreaField, validators, StringField, SubmitField
+from helper_functions_v1 import *
+ 
+# App config.
+DEBUG = True
+app = Flask(__name__)
+app.config.from_object(__name__)
+app.config['SECRET_KEY'] = '7d441f27d441f27567d441f2b6176a'
+ 
+class ReusableForm(Form):
+    name = TextField('Input:', validators=[validators.required()])
+ 
+ 
+@app.route("/", methods=['GET', 'POST'])
+def hello():
+    form = ReusableForm(request.form)
+ 
+    print(form.errors)
+    if request.method == 'POST':
+        sent=request.form['name']
+        
+        sentence_cosine_similarity_list = calculate_sentence_similarity(get_vector_representation(sent)[0])
+        top_five_similar_sentences = find_top_five_max_element_list(sentence_cosine_similarity_list)
+        output = display_items_reversed_order(top_five_similar_sentences)
+        print(output)
+        
+        if form.validate():
+            # Save the comment here.
+            flash(output)
+        else:
+            flash('All the form fields are required. ')
+ 
+    return render_template('index.html', form=form)
+ 
+if __name__ == "__main__":
+    app.run()
